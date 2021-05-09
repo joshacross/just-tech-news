f05ee854293dca6ab8a5e328d77ff22d1099e837
Killa J <joshuaalexandercross@gmail.com>
2021-05-09 15:26:28 -0500

    uddynamically created new columns to posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// define/create model associations
+// This is a onetomany relationship
+User.hasMany(Post, {
+    foreignKey: 'user_id'   
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
 module.exports = { User, Post };
