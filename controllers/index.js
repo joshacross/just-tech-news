2f671c0d63428ed0b43aa4314f5d6c772b76913d
Killa J <joshuaalexandercross@gmail.com>
2021-05-10 22:21:40 -0500

    reformatted code base and required handlebar.js, initialized views

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
 router.use('/api', apiRoutes);
+
+router.use('/', homeRoutes);
 
 // Catch-all if a request is made to any endpoint that does not exist
 router.use((req, res) => {
