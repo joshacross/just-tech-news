45fd982d10bfa7c8c8badd19dd16e6c5a333fca9
Killa J <joshuaalexandercross@gmail.com>
2021-05-11 00:39:26 -0500

    added logout feature

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -103,6 +103,17 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username, email, password}
@@ -146,5 +157,7 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+
 
 module.exports = router;
