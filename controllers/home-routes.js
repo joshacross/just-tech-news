538fa57a587393bd0c11c35701dbe866a5e59c30
Killa J <joshuaalexandercross@gmail.com>
2021-05-10 23:36:50 -0500

    created login page aand js to post user to db

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,18 +36,9 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-    
-//     res.render('homepage', {
-//         id: 1,
-//         post_url: 'https://handlebarsjs.com/guide/',
-//         title: 'Handlebars Docs',
-//         created_at: new Date(),
-//         vote_count: 10,
-//         comments: [{}, {}],
-//         user: {
-//             username: 'test_user'
-//         }
-//     });
-// });
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
 module.exports = router;
